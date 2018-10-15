059bc4f29ce9c59157be0aba3f3da7df6ae4b1cc
Dimanche Jhon Steeve <jsteeve@noukod.com>
2018-10-15 16:19:01 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(tab)
+    tab.select { |e| e if e.chars.sort == @word.chars.sort }
+  end
+end
